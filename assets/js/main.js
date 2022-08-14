bc403fa16efc0549353ee5d8678a8bc4091cfa59
DJTOMATO <48269777+DJTOMATO@users.noreply.github.com>
2022-08-14 15:33:21 -0400

    Ricomida

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function () {
+    $("[id=enviarCorreo]").click(function () {
+        alert("El correo fue enviado correctamente...");
+    });
+});
